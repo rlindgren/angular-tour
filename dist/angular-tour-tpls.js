d0268aba1c8e6a9636b172c56e6fc4ddb52e5c6b
tathata <ryan.lindgren@gmail.com>
2014-04-15 16:07:41 -0400

    tagged commit

--- dist/angular-tour-tpls.js
+++ dist/angular-tour-tpls.js
@@ -5,7 +5,7 @@
  * @author Tyler Henkel
  * @license MIT License, http://www.opensource.org/licenses/MIT
  *
- * Rewritten in part by Ryan Lindgren for knowify LLC
+ * 
  */
 
 (function (window, document, undefined) {
